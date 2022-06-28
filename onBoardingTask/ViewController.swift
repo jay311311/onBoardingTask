7ea45682dcc8106ae9bc0155abaef28866b76424
jay311311 <jay311311@gmail.com>
2022-06-28 09:27:05 +0900

    Initial Commit

---
+++ onBoardingTask/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  onBoardingTask
+//
+//  Created by pineone on 2022/06/28.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
